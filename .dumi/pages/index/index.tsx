67cb46ccdc9a66535faa075b171e1db75e2bfea0
杨文华 <1289978696@qq.com>
2024-02-02 14:37:55 +0800

    fix：首页文字更新

--- .dumi/pages/index/index.tsx
+++ .dumi/pages/index/index.tsx
@@ -9,7 +9,7 @@ import "./index.less";
 const Index: React.FC = () => {
     return <div className="index-container">
         <div className="title-word">ApexTable 1.0</div>
-        <div className="desription">助力开发者「更便捷」地使用可编辑表格，让开发者「不加班」～</div>
+        <div className="desription">干翻友商的「Ant Design Pro 可编辑表格」 </div>
         <div className="header-card"></div>
         <div className="code-link">
             <Button type="link" onClick={() => {
